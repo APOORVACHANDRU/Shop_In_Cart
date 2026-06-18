e9d51ba00657b8ea96fc47be29de744dae080623
Apoorva Chandrashekar <apoorva.rsp@gmail.com>
2026-06-18 10:20:27 +0200

    url updated in frontend

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url = "postgresql://postgres:root@localhost:5432/telusko"
+db_url = "postgresql://neondb_owner:npg_FyYGbt3ZNr2R@ep-nameless-voice-adxnqa0e.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require"
 engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
