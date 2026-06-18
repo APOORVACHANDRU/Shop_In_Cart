330809c2c5c8e19fe2b8b8a9212671c00ebf39a1
Apoorva Chandrashekar <apoorva.rsp@gmail.com>
2026-06-18 11:07:13 +0200

    url added in main.js

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # CORS for React dev server
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://shop-in-cart.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
